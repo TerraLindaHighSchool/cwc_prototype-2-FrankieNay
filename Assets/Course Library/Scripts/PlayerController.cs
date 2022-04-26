b299cbd0248cb58fb0f882d0f5e316a002ba549f
FrankieNay <89489233+FrankieNay@users.noreply.github.com>
2022-04-26 15:39:57 -0700

    Unit 2 Done and adding ammo

---
+++ Assets/Course Library/Scripts/PlayerController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float horizontalInput;
+    public float speed = 10.0f;
+    public float xRange = 20;
+    public float ammo = 10f;
+
+    public GameObject projectilePrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+       //Keep the player in bounds
+        if (transform.position.x < -20)
+        {
+            transform.position = new Vector3(-20, transform.position.y, transform.position.z);
+        }
+        if (transform.position.x > 20)
+        {
+            transform.position = new Vector3(20, transform.position.y, transform.position.z);
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (ammo > 0)
+            {
+                // Launch a projectile from the player
+                Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+                ammo = ammo - 1;
+            }
+        }
+    }
+}
